db97404cd3ce94747dcef6dc356291d8e185bc16
VLT Labs <vltlabs@VLTs-MacBook-Pro.local>
2015-02-14 17:03:30 +0800

    Uploading NSCache project files

--- NSCache/ViewController.m
+++ NSCache/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) NSCache *cacheJSONResponse;
 
 @end
 
@@ -16,12 +17,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self connectToWeatherForecast];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    if (self.cacheJSONResponse == nil)
+    {
+        [self connectToWeatherForecast];
+    }
+    else
+    {
+        NSDictionary *currently = [self.cacheJSONResponse valueForKeyPath:@"currently"];
+        NSLog(@"self.cacheJSONResponse viewWillAppear %@", self.cacheJSONResponse);
+        NSLog(@"currently viewWillAppear %@", currently);
+        
+        //call this to get updated / latest data once the info is shown from self.cacheJSONResponse
+        [self connectToWeatherForecast];
+    }
+
+}
+
+-(void)connectToWeatherForecast
+{
+    NSURL *url = [NSURL URLWithString:@"https://api.forecast.io/forecast/c028fb9d205a3f05aae9fe575dd1d856/37.8267,-122.423"];
+
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+       
+        if (connectionError)
+        {
+            if (self.cacheJSONResponse != nil)
+            {
+                NSDictionary *currently = [self.cacheJSONResponse valueForKeyPath:@"currently"];
+                NSLog(@"self.cacheJSONResponse %@", self.cacheJSONResponse);
+                NSLog(@"currently %@", currently);
+            }
+            else
+            {
+                NSLog(@"connection error with weather forecast");
+            }
+        }
+        else
+        {
+            NSError *error;
+            self.cacheJSONResponse = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+            
+            NSLog(@"cacheJSONData %@", self.cacheJSONResponse);
+            
+        }
+    }];
 }
 
 @end
